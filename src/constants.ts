ca12e6a372f05324dcff3893d9a4873e6225792b
Miftachul Huda <miftachulcc@gmail.com>
2022-11-09 16:41:15 +0700

    change

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,9 @@ export const ALLOWED_ORIGINS = IS_PROD_BUILD
         // ensures that no other sites/apps can communicate with your server
         // whilst you have the app open. If they could (requires an HTTP mitm),
         // they would be able to start proxies & interceptors.
-        /^https:\/\/app\.httptoolkit\.tech$/
+        /^https?:\/\/localhost(:\d+)?$/,
+        /^http:\/\/local\.httptoolkit\.tech(:\d+)?$/,
+        /^https:\/\/app\.httptoolkit\.tech$/,
     ]
     : [
         // Dev builds can use the main site, or local sites, even if those
